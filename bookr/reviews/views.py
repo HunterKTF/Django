307222b0a9b6953f7ab79fdf73e4203a392514d2
Jorge Ricardo Hernández Sabino <jorgericardo98@hotmail.com>
2023-10-18 18:34:04 -0600

    Added variables in index

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    name = request.GET.get("name") or "World"
+    return HttpResponse(f'Hello, {name}!')
+
+
+def login(request):
+    return HttpResponse("Login Page")
+
+
+def register(request):
+    return HttpResponse("Register Page")
